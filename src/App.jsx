fe64bb1205c80cfadfd2c6d192a50d13316d7f08
simone-poli <polismn97@gmail.com>
2025-07-21 11:53:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+
+
+function App() {
+ const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+  return (
+    <>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
