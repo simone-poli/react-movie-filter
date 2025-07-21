1e6eef0dc6f7366868843477b1d17b681201f247
simone-poli <polismn97@gmail.com>
2025-07-21 15:56:32 +0200

    refactor: update title and restructure movie filtering logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useEffect } from 'react'
 
 
 
-function App() {
- const movies = [
+const initialMovies = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -13,10 +12,47 @@ function App() {
   { title: 'Pulp Fiction', genre: 'Thriller' },
 ]
 
+
+
+
+function App() {
+  const [genreFilter, setGenreFilter] = useState('')
+  const [movies, setMovie] = useState(initialMovies)
+
+
+  useEffect(() => {
+    if (genreFilter === '') {
+      setMovie(initialMovies)
+    } else {
+      const filteredMovie = initialMovies.filter(movie => movie.genre.includes(genreFilter)
+      )
+      setMovie(filteredMovie)
+    }
+  })
+
+
+
   return (
     <>
+      <div className="container">
+        <div className="text-center">
+          <input className="form-control m-4" type="text" placeholder='Inserisci genere' value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)} />
+        </div>
 
+        <ul className="list-group m-4">
+          {
+            movies.map((movie, index) => {
+              return (
+                <li key={index} className="list-group-item d-flex justify-content-between">
+                  <p>{movie.title}</p>
+                  <p>{movie.genre}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
 
+      </div>
 
 
 
